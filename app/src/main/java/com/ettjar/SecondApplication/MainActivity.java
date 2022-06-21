efd2bf94acff8871467dc32870634a6385dd593c
Jebreel <jalamari@jebreel.local>
2022-06-21 10:19:22 +0300

    Initial commit

---
+++ app/src/main/java/com/ettjar/SecondApplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ettjar.SecondApplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
